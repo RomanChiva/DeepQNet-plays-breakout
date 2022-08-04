8a39d5132896400089184c053f08eed2944b7326
RomanChiva <rchivag@gmail.com>
2022-08-04 18:59:12 +0200

    Feature Extractor Improved

--- feature_extractor.py
+++ feature_extractor.py
@@ -3,6 +3,10 @@ import numpy as np
 import gym
 import matplotlib.pyplot as plt
 from skimage import color, transform
+from sklearn.cluster import DBSCAN
+from sklearn.feature_extraction import image
+import copy
+
 
 # Make the obervations from the environment more compact
 def feat_extract(obs):
@@ -11,20 +15,50 @@ def feat_extract(obs):
     # Convert to grayscale
     obs = color.rgb2gray(obs)
     # Rescale
-    obs = transform.rescale(obs,0.6,anti_aliasing=True)
+    obs = transform.rescale(obs,1,anti_aliasing=True)
     # Return, rescaled cropped grayscale observation
     return obs
 
+
+# Make the obervations from the environment more compact
+def feat_extract2(obs):
+    #Crop area of interest
+    obs_r = obs[52:196,9:152,:]
+    obs_r = color.rgb2gray(obs_r)
+    obs_r[obs_r != 0] = 255
+    # # Rescale
+    obs_r = transform.rescale(obs_r,0.5,anti_aliasing=True)
+    
+
+    # ==== FIND CENTROIDS
+    non0 = np.transpose(np.nonzero(obs_r))
+
+    dbscan = DBSCAN(eps=3,min_samples=1).fit(non0)
+    labels = dbscan.labels_
+
+    len0 = len(non0[labels==0])
+    len1 = len(non0[labels==1])
+    len2 = len(non0[labels==2])
+
+    centroid_0 = np.append(np.mean(non0[labels==0],axis=0),len0)
+    centroid_1 = np.append(np.mean(non0[labels==1],axis=0),len1)
+    centroid_2 = np.append(np.mean(non0[labels==2],axis=0),len2)
+
+    centroids = np.array([centroid_0,centroid_1,centroid_2])
+    centroids = centroids[centroids[:,2].argsort()]
+
+    return centroids
 
 # Check what ur function is doing. If you mand to mod for other games or smth
 if __name__ =='__main__':
     env = gym.make('Breakout-v4')
     obs = env.reset()
-    obs = feat_extract(obs)
+    #obs = feat_extract(obs)
     obs1,a,b,_ = env.step(1)
-    obs2,a,b,_ = env.step(2)
-    obs2 = feat_extract(obs2)
-    print(obs.shape)
-    plt.imshow(obs2, cmap='Greys')
+    obs2,a,b,_ = env.step(2) 
+    obs3,a,b,_ = env.step(2)
+    centroids = feat_extract2(obs2)
+    print(centroids)
+    #plt.imshow(obs2, cmap='Greys')
     
-    plt.show()
+    #plt.show()
